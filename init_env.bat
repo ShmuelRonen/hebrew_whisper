105829563301d3ec8ab435ee7a1c918e411a77e0
Shmuel Ronen <80190186+ShmuelRonen@users.noreply.github.com>
2024-03-31 22:11:13 +0300

    Add files via upload

---
+++ init_env.bat
@@ -0,0 +1,20 @@
+@echo off
+
+rem Check if virtual environment exists
+if not exist "venv" (
+    echo Creating virtual environment...
+    python -m venv venv
+    echo Virtual environment created.
+)
+
+rem Activate the virtual environment
+call venv\Scripts\activate
+
+rem Install requirements
+echo Installing requirements...
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install -r requirements.txt
+echo Requirements installed.
+
+rem Run the Python script
+python app.py
