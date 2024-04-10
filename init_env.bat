c1827df53825b9b3a9f3ac0e7e248d0d1fb253b6
Shmuel Ronen <80190186+ShmuelRonen@users.noreply.github.com>
2024-04-10 15:08:13 +0300

    Add git pull

--- init_env.bat
+++ init_env.bat
@@ -5,19 +5,23 @@ if not exist "venv" (
     echo Creating virtual environment...
     python -m venv venv
     echo Virtual environment created.
-
-    rem Activate the virtual environment
-    call venv\Scripts\activate
-
-    rem Install requirements
-    echo Installing requirements...
-    pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-    pip install -r requirements.txt
-    echo Requirements installed.
-) else (
-    rem Activate the virtual environment
-    call venv\Scripts\activate
 )
+
+rem Activate the virtual environment
+call venv\Scripts\activate
+
+rem Perform git pull to update the repository
+echo Updating the repository...
+git pull
+echo Repository updated.
+
+rem Install requirements
+echo Installing requirements...
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install -r requirements.txt
+echo Requirements installed.
 
 rem Run the Python script
 python app.py
+
+pause
